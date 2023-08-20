727d829f0c621654024a8b88afa603447e8c61c8
Kevin Parra <kevinparra535@gmail.com>
2023-08-19 21:03:06 -0500

    [ADD] REACT NAVIGATION

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
 
-import NotesScreen from './ui/screens/Notes/NotesScreen';
+import NotesScreen from "./ui/screens/Notes/NotesScreen";
 
 export default function App() {
   return (
@@ -15,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
